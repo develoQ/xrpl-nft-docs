3fa740ea63a66120f54302a4087e7c9e431879b0
tequ <git@tequ.dev>
2023-07-18 21:55:54 +0900

    first commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>XRPL NFTドキュメント</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/develoQ/xrpl-nft-docs",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.gg/64ad9mc5N6",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/develoQ/xrpl-nft-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Provided by XRPL NFT Japan Community",
   },
-}
+};
 
 export default config
