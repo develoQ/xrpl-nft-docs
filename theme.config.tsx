9ab0f6166eea5a29d039164f4a4135492ee0c002
develoQ <develoQ.jp@gmail.com>
2023-08-01 14:20:51 +0900

    add title template

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - XRPL NFTドキュメント',
+    }
+  },
   logo: <span>XRPL NFTドキュメント</span>,
   project: {
     link: "https://github.com/develoQ/xrpl-nft-docs",
