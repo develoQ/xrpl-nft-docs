12ee50e5682596f64dd9cb22058db718f4daa71f
tequ <git@tequ.dev>
2023-08-31 09:46:44 +0900

    add metadata

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,6 +7,7 @@ const config: DocsThemeConfig = {
       titleTemplate: '%s - XRPL NFTドキュメント',
     }
   },
+
   logo: <span>XRPL NFTドキュメント</span>,
   project: {
     link: "https://github.com/develoQ/xrpl-nft-docs",
@@ -15,6 +16,19 @@ const config: DocsThemeConfig = {
     link: "https://discord.gg/7Q3MUuTRer",
   },
   docsRepositoryBase: "https://github.com/develoQ/xrpl-nft-docs",
+  head: <>
+    <title>XRPL NFTドキュメント</title>
+    <meta name="description" content="コミュニティによるXRPL NFTの日本語ドキュメントです！" />
+    <meta property="og:title" content="XRPL NFTドキュメント" />
+    <meta property="og:description" content="コミュニティによるXRPL NFTの日本語ドキュメントです！" />
+    <meta property="og:url" content="https://xrpl-nft-docs-jp.vercel.app" />
+    <meta property="og:image" content="https://xrpl-nft-docs-jp.vercel.app/ogp.png" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="XRPL NFTドキュメント" />
+    <meta name="twitter:description" content="コミュニティによるXRPL NFTの日本語ドキュメントです！" />
+    <link rel="icon" href="/favicon.ico" type="image/png" sizes="256x256"/>
+  </>,
   footer: {
     text: "Provided by XRPL NFT Japan Community",
   },
